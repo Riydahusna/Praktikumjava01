644a0e0e8078a7d23b16f364c792de0b77e0228c
Riydahusna <Riydahusna20@gmail.com>
2021-06-06 20:43:16 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+	String namaDepan = "Riyda";
+	String namaTengah = "-";
+	String namaBelakang = "Husna";
+	int usia = 18;
+	int targetTahunKuliah = 4;
+	double ipk = 3.89764512;
+	char nilaiAbjad = 'A';
+	boolean cantik = true;
+ }
+}
