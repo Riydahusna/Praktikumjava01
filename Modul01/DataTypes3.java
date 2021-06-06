1d7f94d7fd133f6740fb607f3b3a1cb4c9e791c0
Riydahusna <Riydahusna20@gmail.com>
2021-06-06 20:51:42 +0800

    Output dengan MessageDialog

---
+++ Modul01/DataTypes3.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class DataTypes3{
+ public static void main(String[] args){
+
+	String namaDepan = "Riyda";
+	String namaBelakang = "Husna";
+	int usia = 18;
+	int targetTahunKuliah = 4;
+	double ipk = 3.89764512;
+	char nilaiAbjad = 'A';
+	boolean cantik = true;
+
+	System.out.println("Nama depan : " + namaDepan);
+	System.out.println("Nama belakang : " + namaBelakang);
+	System.out.println("Usia : " + usia);
+	System.out.println("Target Kuliah : " + targetTahunKuliah);
+	System.out.println("IPK : " + ipk);
+	System.out.println("Nilai PBO : " + nilaiAbjad);
+	System.err.println("Cantik : " + cantik);
+
+	JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaBelakang);
+
+ }
+}
