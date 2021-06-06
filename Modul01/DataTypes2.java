f906e13ce7b3b8afe5eb35939704a5a7410b6ad5
Riydahusna <Riydahusna20@gmail.com>
2021-06-06 20:46:59 +0800

    Output dengan sout

---
+++ Modul01/DataTypes2.java
@@ -0,0 +1,20 @@
+public class DataTypes2{
+ public static void main(String[] args){
+
+	String namaDepan = "Riyda";
+	String namaBelakang = "Husna";
+	int usia = 18;
+	int targetTahunKuliah = 4;
+	double ipk = 3.89764512;
+	char nilaiAbjad = 'A';
+	boolean cantik = true;
+
+	System.out.println("Nama depan : " + namaDepan);
+	System.out.println("Nama belakang : " + namaBelakang);
+	System.out.println("Usia : " + usia);
+	System.out.println("Target Kuliah : " + targetTahunKuliah);
+	System.out.println("IPK : " + ipk);
+	System.out.println("Nilai PBO : " + nilaiAbjad);
+	System.err.println("Cantik : " + cantik);
+ }
+}
