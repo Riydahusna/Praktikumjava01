7cdb20d9e4fd6dae32818ae71c9f1c086de4c8b1
Riydahusna <Riydahusna20@gmail.com>
2021-06-12 21:27:27 +0800

    modul 2 IDEA

---
+++ Modul_02/src/Main.java
@@ -0,0 +1,10 @@
+import model.Mahasiswa;
+
+public class Main {
+	public static void main(String[] args) {
+	  Mahasiswa m1 = new Mahasiswa ("2010010816","Riyda","05-02-1999");
+	  m1.tampilkanAtribut();
+	  System.out.println(m1.getNama());
+
+	}
+}
